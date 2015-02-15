f4af59b0bffccefbf6794a92f960fba7d37a7a77
Maddy Lau <maddylau@gmail.com>
2015-02-15 17:55:11 -0500

    RESTful actions and routes for Posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,46 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    # binding.pry
+    @post = Post.find_by_id(params[:id])
+  end
+
+  def new
+  end  
+
+  def create 
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = "Post was successfully saved!"
+      redirect_to :posts
+    end
+  end
+
+  def edit
+    @post = Post.find_by_id(params[:id])
+  end
+
+  def update
+    @post = Post.find_by_id(params[:id])
+    if @post.update_attributes(post_params)
+      flash[:notice] = "Updated Successfully!"
+      redirect_to :posts
+    end
+  end
+
+  def destroy
+    @post = Post.find_by_id(params[:id])
+    @post.destroy
+    flash[:notice] = "Student was destroyed"
+    redirect_to :posts
+  end
+
+
+  private
+    def post_params
+      params.require(:post).permit(:headline, :body)
+    end
+end
